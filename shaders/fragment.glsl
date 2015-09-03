599f1d1d8c9f559800244c3a53e5f3b8671c7e40
gnishida <gnishida@purdue.edu>
2015-09-03 12:54:02 -0400

    some refinements

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,10 +1,10 @@
 #version 330
 
 // varying variables
-in vec3 fColor;
-in vec3 fTexCoord;
-in vec3 fNormal;
-in vec3 fPosition;
+in vec3 vColor;
+in vec3 vTexCoord;
+in vec3 vNormal;
+in vec3 vPosition;
 
 // output color
 out vec4 outputF;
@@ -14,7 +14,6 @@ uniform int pass;	// 1 -- 1st pass / 2 -- 2nd pass
 // uniform variables
 uniform int textureEnabled;	// 0 -- color / 1 -- texture
 uniform sampler2D tex0;
-uniform int wireframeEnalbed; // 0 -- no wireframe / 1 -- add wireframe
 
 uniform vec3 lightDir;
 uniform sampler2D normalMap;
@@ -23,16 +22,16 @@ uniform int screenWidth;
 uniform int screenHeight;
 
 // return random value in [-1, 1]
-float random(float seed, float scale, int cycle_size) {
-	return float(int(seed * scale) % (cycle_size * 2 + 1) - cycle_size) / float(cycle_size);
+float random(vec3 position, float scale, int cycle_size) {
+	return float(int((position.x + position.y + position.z) * scale) % (cycle_size * 2 + 1) - cycle_size) / float(cycle_size);
 }
 
 void main() {
 	if (pass == 1) {
-		outputF = vec4((fNormal + 1) * 0.5, 1);
+		outputF = vec4((vNormal + 1) * 0.5, 1);
 	} else {
 		// for color mode
-		outputF = vec4(fColor, 1.0);
+		outputF = vec4(vColor, 1.0);
 
 		float diff = 0;
 		int range = 1;
@@ -44,9 +43,9 @@ void main() {
 		int cycle_size[5] = int[](73, 193, 311, 117, 213);
 		float jitter_size = 3.0;
 		for (int iter = 0; iter < num_iterations; ++iter) {
-			float sx = gl_FragCoord.x + random(fPosition.x + fPosition.y + fPosition.z, scale_x[iter], cycle_size[iter]) * jitter_size;
-			float sy = gl_FragCoord.y + random(fPosition.x + fPosition.y + fPosition.z, scale_y[iter], cycle_size[iter]) * jitter_size;
-			float sz = gl_FragCoord.z + random(fPosition.x + fPosition.y + fPosition.z, scale_z[iter], cycle_size[iter]) * jitter_size;
+			float sx = gl_FragCoord.x + random(vPosition, scale_x[iter], cycle_size[iter]) * jitter_size;
+			float sy = gl_FragCoord.y + random(vPosition, scale_y[iter], cycle_size[iter]) * jitter_size;
+			float sz = gl_FragCoord.z + random(vPosition, scale_z[iter], cycle_size[iter]) * jitter_size;
 
 			// difference in normal between this pixel and the neighbor pixels
 			vec3 n = texture(normalMap, vec2(sx / screenWidth, sy / screenHeight)).xyz;
@@ -77,18 +76,17 @@ void main() {
 		}
 		outputF = vec4(1 - diff, 1 - diff, 1 - diff, 1);
 		
-		//outputF = texture(normalMap, vec2(gl_FragCoord.x / screenWidth, gl_FragCoord.y / screenHeight));
 		return;
 
 	
 
 		if (textureEnabled == 1) { // for texture mode
-			outputF = texture(tex0, fTexCoord.rg);
+			outputF = texture(tex0, vTexCoord.rg);
 		}
 
 		// lighting
 		vec4 ambient = vec4(0.3, 0.3, 0.3, 1.0);
-		vec4 diffuse = vec4(0.7, 0.7, 0.7, 1.0) * max(0.0, dot(-lightDir, fNormal));
+		vec4 diffuse = vec4(0.7, 0.7, 0.7, 1.0) * max(0.0, dot(-lightDir, vNormal));
 
 		outputF = (ambient + diffuse) * outputF;
 	}
